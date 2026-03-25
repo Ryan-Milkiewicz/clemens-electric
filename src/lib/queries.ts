965525234acadde864515a8919bb163ccb3221d5
Ryan <ryanmilkiewicz@gmail.com>
2026-03-25 11:30:27 -0400

    updated revalidation and used sanity cdn

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -22,48 +22,61 @@ import {
   ALL_BLOG_POST_QUERY,
 } from "@/sanity/queries";
 
-const options = { next: { revalidate: 30 } };
+const staticOptions = { next: { revalidate: 86400 } }; // 24h - rarely changes
+const contentOptions = { next: { revalidate: 3600 } }; // 1h - moderate changes
 
 export async function getHeaderSection() {
-  return await client.fetch<HeroSection>(HERO_QUERY, {}, options);
+  return await client.fetch<HeroSection>(HERO_QUERY, {}, staticOptions);
 }
 
 export async function getAboutSection() {
-  return await client.fetch<AboutSection>(ABOUT_QUERY, {}, options);
+  return await client.fetch<AboutSection>(ABOUT_QUERY, {}, staticOptions);
 }
 
 export async function getServices() {
-  return await client.fetch<Services[]>(SERVICE_QUERY, {}, options);
+  return await client.fetch<Services[]>(SERVICE_QUERY, {}, staticOptions);
 }
 
 export async function getServicesBySlug(slug: string) {
-  return await client.fetch<Services>(GET_SERVICE_BY_SLUG, { slug }, options);
+  return await client.fetch<Services>(
+    GET_SERVICE_BY_SLUG,
+    { slug },
+    staticOptions,
+  );
 }
 
 export async function getMarketsSection() {
-  return await client.fetch<MarketSection>(MARKET_QUERY, {}, options);
+  return await client.fetch<MarketSection>(MARKET_QUERY, {}, staticOptions);
 }
 
 export async function getFAQSection() {
-  return await client.fetch<FAQSection>(FAQ_Query, {}, options);
+  return await client.fetch<FAQSection>(FAQ_Query, {}, staticOptions);
 }
 
 export async function getTop3BlogPosts() {
-  return await client.fetch<Post[]>(LATEST_BLOG_POSTS_QUERY, {}, options);
+  return await client.fetch<Post[]>(
+    LATEST_BLOG_POSTS_QUERY,
+    {},
+    contentOptions,
+  );
 }
 
 export async function getBlogPost(slug: string) {
-  return await client.fetch<FullPost>(BLOG_POST_QUERY, { slug }, options);
+  return await client.fetch<FullPost>(
+    BLOG_POST_QUERY,
+    { slug },
+    contentOptions,
+  );
 }
 
 export async function getAllBlogPosts() {
-  return await client.fetch<Post[]>(ALL_BLOG_POST_QUERY, {}, options);
+  return await client.fetch<Post[]>(ALL_BLOG_POST_QUERY, {}, contentOptions);
 }
 
 export async function getDynamicPage(pageType: string) {
   return await client.fetch<DynamicPages>(
     DYNAMIC_PAGE_QUERY,
     { pageType },
-    options,
+    staticOptions,
   );
 }
